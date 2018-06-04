b35cceae55449b2a66503b85f914fb1770374815
ikuts <ikuts@tsconsulting.ru>
2018-06-04 19:42:45 +0300

    template

---
+++ src/main/java/ru/tsconsulting/controller/Controller.java
@@ -0,0 +1,11 @@
+package ru.tsconsulting.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+    @RequestMapping("/hello")
+    public String showHello(){
+        return "Hello";
+    }
+}
---
+++ src/main/resources/templates/hello.ftl
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h1>Hello flt</h1>
+</body>
+</html>
