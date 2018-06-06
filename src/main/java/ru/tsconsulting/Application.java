0a5b535a4ad4fda89fa33a50e77ed658c78a7f7e
ikuts <ikuts@tsconsulting.ru>
2018-06-06 22:48:27 +0300

    uri fix

--- src/main/java/ru/tsconsulting/Application.java
+++ src/main/java/ru/tsconsulting/Application.java
@@ -7,11 +7,20 @@ import org.springframework.context.annotation.Bean;
 import ru.tsconsulting.soap.SOAPConnector;
 import ru.tsconsulting.soap.model.ya.CheckTextRequest;
 import ru.tsconsulting.soap.model.ya.CheckTextResponse;
+import ru.tsconsulting.soap.model.ya.SpellError;
+import ru.tsconsulting.soap.model.ya.SpellResult;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
 
 
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(Application.class, args);
     }
 
@@ -19,15 +28,21 @@ public class Application {
     @Bean
     CommandLineRunner lookup(SOAPConnector soapConnector) {
         return strings -> {
-            CheckTextRequest request;
             try {
-                request = new CheckTextRequest();
+                CheckTextRequest request = new CheckTextRequest();
                 request.setText("Домек");
                 request.setLang("ru");
-                CheckTextResponse response = (CheckTextResponse) soapConnector.callWebService("https://speller.yandex.net/services/spellservice", request);
-                System.out.println(response.getSpellResult());
+                CheckTextResponse response = (CheckTextResponse) soapConnector.callWebService("http://speller.yandex.net/services/spellservice/checkText", request);
+                System.out.println("*********************************************************************");
+                SpellResult spellResult = response.getSpellResult();
+                List<SpellError> errors = spellResult.getError();
+                for (SpellError error : errors) {
+                    System.out.println("Варианты написания:");
+                    error.getS().forEach(System.out::println);
+                }
+                System.out.println("*********************************************************************");
             } catch (Exception e) {
-                System.out.println(e);
+                e.printStackTrace();
             }
         };
     }
